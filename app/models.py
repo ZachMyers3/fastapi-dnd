dfe8c19b0ab84074d239f15c18d5c253bff897ec
ZachMyers3 <ZachMyers3@gmail.com>
2019-12-09 10:27:56 -0500

    added to CharacterSchema

--- app/models.py
+++ app/models.py
@@ -6,6 +6,18 @@ mongo = PyMongo()
 class CharacterSchema(Schema):
   firstName = fields.Str()
   lastName = fields.Str()
-  
+  # base stats
+  strength = fields.Int()
+  dexterity = fields.Int()
+  constitution = fields.Int()
+  intelligence = fields.Int()
+  wisdom = fields.Int()
+  charisma = fields.Int()
+  # vitals
+  currentHP = fields.Int()
+  maxHP = fields.Int()
+  currentAC = fields.Int()
+  baseAC = fields.Int()
+   
  class PartySchema(Schema):
   members = fields.List(fields.Nested(CharacterSchema), required=True)
