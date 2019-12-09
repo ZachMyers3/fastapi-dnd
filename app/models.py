51acfa3e4f1c725fea43f638719ad19a57dd277c
ZachMyers3 <ZachMyers3@gmail.com>
2019-12-09 10:23:39 -0500

    added party schema

--- app/models.py
+++ app/models.py
@@ -6,3 +6,6 @@ mongo = PyMongo()
 class CharacterSchema(Schema):
   firstName = fields.Str()
   lastName = fields.Str()
+  
+ class PartySchema(Schema):
+  members = fields.List(fields.Nested(CharacterSchema), required=True)
