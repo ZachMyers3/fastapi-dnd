3e0d2616a2b3a5175e1a5c09b0971b34fbe2f22b
ZachMyers3 <ZachMyers3@gmail.com>
2019-12-09 08:25:29 -0500

    added marshmellow schema

--- app/models.py
+++ app/models.py
@@ -1,3 +1,8 @@
 from flask_pymongo import PyMongo
+from marshmellow import Schema, fields
 
 mongo = PyMongo()
+
+class CharacterSchema(Schema):
+  firstName = fields.Str()
+  lastName = fields.Str()
