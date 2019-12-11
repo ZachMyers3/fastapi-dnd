984eeed16457177a13ee6f96fbf9ba0ee8d309f7
Zach Myers <zachmyers@woosterbrush.com>
2019-12-11 08:43:28 -0500

    removed print message

--- app/views.py
+++ app/views.py
@@ -12,10 +12,8 @@ def get_character():
     # need first name
     if not 'firstName' in request.json:
         return jsonify(ok=False, msg='First name required'), 400
-    print(f'{jsonify(request.args)=}')
     # based off of the JSON find character
     this_char = mongo.db.characters.find_one(request.json)
-    print(f'{this_char=}')
     if this_char:
         return jsonify(ok=True, character=this_char)
     else:
@@ -30,7 +28,6 @@ def create_character():
     pid = characters.insert(request.json)
     # find new player
     new_char = characters.find_one({'_id': pid})
-    print(f'{new_char}')
     return jsonify(ok=True, character=new_char)
 
 @views.route('/character', methods=['PUT'])
@@ -43,11 +40,9 @@ def update_character():
     # find the object by id and update from the rest of the json
     id_json = {'_id': request.json['_id']}
     update_char = mongo.db.characters.update_one(id_json, request.json['$set'])
-    print(f'{update_char=}')
     this_char = mongo.db.characters.find_one({'_id': request.json['_id']})
-    print(f'{this_char=}')
 
-    return jsonify(ok=True)
+    return jsonify(ok=True, character=this_char)
 
 @views.route('/characters', methods=['GET'])
 def get_characters():
