b25b7fef4b202e42bc131f74fca7e44c4cc66bca
Zach Myers <zachmyers@woosterbrush.com>
2021-02-25 17:04:15 -0500

    migrate to poetry

--- config.py
+++ config.py
@@ -1,13 +1,17 @@
 """ Flask config class """
 import os
+from dotenv import find_dotenv, load_dotenv
+
+load_dotenv(find_dotenv())
+
 
 class Config(object):
     # general config
-    TESTING = bool(os.environ.get('TESTING'))
-    DEBUG = bool(os.environ.get('DEBUG'))
-    FLASK_ENV = os.environ.get('FLASK_ENV')
+    TESTING = bool(os.environ.get("TESTING"))
+    DEBUG = bool(os.environ.get("DEBUG"))
+    FLASK_ENV = os.environ.get("FLASK_ENV")
     # flask-pymongo
-    MONGO_URI = os.environ.get('MONGODB_URI')
+    MONGO_URI = os.environ.get("MONGODB_URI")
     # MONGO_URI = os.environ.get('MONGO_URI')
 
     SCHEDULER_API_ENABLED = True
