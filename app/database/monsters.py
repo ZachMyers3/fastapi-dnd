8b60bd0f2e0917c282e3feab30fbc2e62cda598b
Zach Myers <zachmyers@woosterbrush.com>
2021-02-26 16:23:09 -0500

    create unique monster call

--- app/database/monsters.py
+++ app/database/monsters.py
@@ -1,5 +1,16 @@
+from bson.objectid import ObjectId
+
 from .database import db
 
 
 def retrieve_monsters():
     return db.monsters.find()
+
+
+def retrieve_monster(_id: str):
+    try:
+        _id = ObjectId(_id)
+    except:
+        return None
+    finally:
+        return db.monsters.find_one({"_id": _id})
--- app/routers/monsters.py
+++ app/routers/monsters.py
@@ -1,7 +1,7 @@
 from typing import List
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from ..schema.monsters import MonsterSchema
-from ..database.monsters import retrieve_monsters
+from ..database.monsters import retrieve_monster, retrieve_monsters
 
 router = APIRouter()
 
@@ -9,3 +9,11 @@ router = APIRouter()
 @router.get("/monsters/", response_model=List[MonsterSchema])
 def get_all_equipment():
     return list(retrieve_monsters())
+
+
+@router.get("/monsters/{spell_id}/", response_model=MonsterSchema)
+def get_spell_by_id(_id: str):
+    result = retrieve_monster(_id=_id)
+    if result is None:
+        HTTPException(status_code=404, detail="Object not found")
+    return result
