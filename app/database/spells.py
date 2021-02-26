3ef2514750039e55e12f2154e4a5287ac355e06e
Zach Myers <zachmyers@woosterbrush.com>
2021-02-26 16:15:36 -0500

    add singular spell lookup

--- app/database/spells.py
+++ app/database/spells.py
@@ -1,5 +1,11 @@
+from bson.objectid import ObjectId
+
 from .database import db
 
 
 def retrieve_spells():
     return db.spells.find()
+
+
+def retrieve_spell(_id: ObjectId):
+    return db.spells.find_one({"_id": _id})
--- app/routers/spells.py
+++ app/routers/spells.py
@@ -1,10 +1,11 @@
 from typing import List
+from bson.objectid import ObjectId
 
 from fastapi import Depends, APIRouter, HTTPException
 
 from ..schema.spells import SpellSchema
 
-from ..database.spells import retrieve_spells
+from ..database.spells import retrieve_spell, retrieve_spells
 
 router = APIRouter()
 
@@ -12,3 +13,12 @@ router = APIRouter()
 @router.get("/spells/", response_model=List[SpellSchema])
 def get_all_spells():
     return list(retrieve_spells())
+
+
+@router.get("/spell/{spell_id}/", response_model=SpellSchema)
+def get_spell_by_id(spell_id: str):
+    print(f"Getting spell by id {spell_id}")
+    result = retrieve_spell(_id=ObjectId(spell_id))
+    if result is None:
+        HTTPException(status_code=404, detail="Object not found")
+    return result
